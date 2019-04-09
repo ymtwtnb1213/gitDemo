6b7911aea1b1c59305de9a08099e814ceaffdf6b
itshattori <ryohei.hattori@accenture.com>
2019-04-09 14:11:36 +0900

    テストメソッドの削除_20190409_itshattori

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
-    public String testMethod() {
-        return "テストメソッド";
-    }
 }
