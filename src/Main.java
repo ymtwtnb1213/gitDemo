47f7eaa33afc49023541b0de000d82302781610b
itshattori <ryohei.hattori@accenture.com>
2019-04-09 14:17:01 +0900

    pullデモ用メソッド

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    /**
+     * プルデモ用メソッド
+     * */
+    public void pullDemoMethod() {
+        System.out.println("pullデモ用に作成したメソッドです。");
+    }
 }
